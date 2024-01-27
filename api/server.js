6192d2bb8003529694b31ccd18c4a35bb0114c08
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-27 15:47:29 -0800

    dependency updates

--- api/server.js
+++ api/server.js
@@ -6,3 +6,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose
+    .connect('mongodb://localhost:27017/mern-todo', {
+        useNewUrlParser: true,
+        useUnifiedTopolgy: true,
+    })
+    .then(() => console.log('Connected to DB'))
+    .catch(console.error);
+
+app.listen(3001, () => console.log('Server started on Port 3001'));
