ee708eeea35009c7372dc0aadf2eb8525336c110
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-27 18:37:37 -0800

    created Express route to listen for HTTP GET requests at the path '/todos' Handler function uses Mongoose to query the database and return a json res back to client with an array of todo tasks

--- api/server.js
+++ api/server.js
@@ -2,14 +2,26 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+// EXPRESS
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+//
 
+// MongoDB
 mongoose
     .connect('mongodb://127.0.0.1:27017/mern-todo')
     .then(() => console.log('Connected to DB'))
     .catch(console.error);
 
+import { Todo } from './models/Todo';
+
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+//
+
+// SERVER/PORT
 app.listen(3001, () => console.log('Server started on Port 3001'));
