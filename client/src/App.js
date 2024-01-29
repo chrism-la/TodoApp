bef2f1ad29e3c15038d914393376e711c18af1f9
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-28 17:04:28 -0800

    created boilerplate JSX to view elements and style the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>Welcome, Chris</h1>
+            <h4>Your Tasks</h4>
+            <div className="todos">
+                <div className="todo">
+                    <div className="checkbox"></div>
+                    <div className="text">Get the Bread</div>
+                    <div className="delete-todo">x</div>
+                </div>
+                <div className="todo is-complete">
+                    <div className="checkbox"></div>
+                    <div className="text">Get the Milk</div>
+                    <div className="delete-todo">x</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
